c071c8eda9349bfa582d5c6cf565d2b123f73a6d
binariWork <nearbirddd@mail.ru>
2025-11-26 12:40:55 +0300

    Created map section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 //Burger button animation and property
 document.querySelector(".burger").addEventListener('click', function() {
     this.classList.toggle('active');
-})
+});
+
+//miniMap
+function init () {
+    let map = new ymaps.Map('map', {
+        center: [45.043499, 41.962087],
+        zoom: 18
+    });
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+ymaps.ready(init);
